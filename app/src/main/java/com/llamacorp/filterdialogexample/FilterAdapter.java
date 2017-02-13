d52d564d189f43c5e977f0be31f12e27d8806a04
EvanRespaut <evanrespaut@gmail.com>
2017-02-13 01:12:48 -0500

    Made filtering much more robust, will now order results intelligently.  Cleaned up code.

--- app/src/main/java/com/llamacorp/filterdialogexample/FilterAdapter.java
+++ app/src/main/java/com/llamacorp/filterdialogexample/FilterAdapter.java
@@ -11,107 +11,131 @@ import android.widget.Filterable;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.LinkedHashSet;
 import java.util.List;
 
 /**
  * Custom adapter class that implements the Filterable interface, allowing it
  * to filter items within it based on a filter string.
  */
-public class FilterAdapter extends BaseAdapter implements Filterable {
-   List<String> arrayList; // current values post filtering
-   private List<String> mOriginalValues; // values pre filtering
-   private LayoutInflater inflater;
+class FilterAdapter extends BaseAdapter implements Filterable {
+	List<String> arrayList; // current values post filtering
+	private List<String> mOriginalValues; // values pre filtering
+	private LayoutInflater inflater;
 
-   public FilterAdapter(Context context, List<String> arrayList) {
-      this.arrayList = arrayList;
-      inflater = LayoutInflater.from(context);
-   }
+	FilterAdapter(Context context, List<String> arrayList) {
+		this.arrayList = arrayList;
+		inflater = LayoutInflater.from(context);
+	}
 
-   @Override
-   public int getCount() {
-      return arrayList.size();
-   }
+	@Override
+	public int getCount() {
+		return arrayList.size();
+	}
 
-   @Override
-   public Object getItem(int position) {
-      return position;
-   }
+	@Override
+	public Object getItem(int position) {
+		return position;
+	}
 
-   @Override
-   public long getItemId(int position) {
-      return position;
-   }
+	@Override
+	public long getItemId(int position) {
+		return position;
+	}
 
-   private class ViewHolder {
-      TextView textView;
-   }
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		ViewHolder holder;
 
-   @Override
-   public View getView(int position, View convertView, ViewGroup parent) {
-      ViewHolder holder;
+		if (convertView == null){
 
-      if (convertView == null){
+			holder = new ViewHolder();
+			convertView = inflater.inflate(R.layout.dialog_list_row, parent, false);
+			holder.textView = (TextView) convertView.findViewById(R.id.country_name_textView);
+			convertView.setTag(holder);
+		} else {
+			holder = (ViewHolder) convertView.getTag();
+		}
+		holder.textView.setText(arrayList.get(position));
+		return convertView;
+	}
 
-         holder = new ViewHolder();
-         convertView = inflater.inflate(R.layout.alertlistrow, null);
-         holder.textView = (TextView) convertView.findViewById(R.id.country_name_textView);
-         convertView.setTag(holder);
-      } else {
-         holder = (ViewHolder) convertView.getTag();
-      }
-      holder.textView.setText(arrayList.get(position));
-      return convertView;
-   }
+	@Override
+	public Filter getFilter() {
+		return new Filter() {
 
-   @Override
-   public Filter getFilter() {
-      return new Filter() {
+			@SuppressWarnings("unchecked")
+			@Override
+			protected void publishResults(CharSequence constraint, FilterResults results) {
+				arrayList = (List<String>) results.values; // has the filtered values
+				notifyDataSetChanged();  // notifies the data with new filtered values
+			}
 
-         @SuppressWarnings("unchecked")
-         @Override
-         protected void publishResults(CharSequence constraint, FilterResults results) {
-            arrayList = (List<String>) results.values; // has the filtered values
-            notifyDataSetChanged();  // notifies the data with new filtered values
-         }
+			@Override
+			protected FilterResults performFiltering(CharSequence constraint) {
+				FilterResults results = new FilterResults();        // Holds the results of a filtering operation in values
 
-         @Override
-         protected FilterResults performFiltering(CharSequence constraint) {
-            FilterResults results = new FilterResults();        // Holds the results of a filtering operation in values
-            List<String> FilteredArrList = new ArrayList<>();
+				/* Create a hierarchy or order results separated by different sets
+				* and then added together at the end.  Note HashSet used to guarantee
+				* no duplicate results */
+				LinkedHashSet<String> mainList = new LinkedHashSet<>();
+				LinkedHashSet<String> secondaryList = new LinkedHashSet<>();
+				LinkedHashSet<String> tertiaryList = new LinkedHashSet<>();
 
-            if (mOriginalValues == null){
-               mOriginalValues = new ArrayList<>(arrayList); // saves the original data in mOriginalValues
-            }
+				if (mOriginalValues == null){
+					mOriginalValues = new ArrayList<>(arrayList); // saves the original data in mOriginalValues
+				}
 
-            // if constraint is too short:
-            if (constraint == null || constraint.length() == 0){
-               // set the Original result to return
-               results.count = mOriginalValues.size();
-               results.values = mOriginalValues;
-            // constraint is long enough, proceed:
-            } else {
-               constraint = constraint.toString().toLowerCase();
-               int mTextLength = constraint.length();
+				// if constraint is too short:
+				if (constraint == null || constraint.length() == 0){
+					// set the Original result to return
+					results.count = mOriginalValues.size();
+					results.values = mOriginalValues;
+					// constraint is long enough, proceed:
+				} else {
+					constraint = constraint.toString().toLowerCase();
+					int mTextLength = constraint.length();
 
-               for (String data : mOriginalValues) {
-                  if (mTextLength > data.length())
-                     continue;
-                  if (data.toLowerCase().contains(constraint)){
-                     FilteredArrList.add(data);
-                  }
-               }
+					mainLoop:
+					for (String data : mOriginalValues) {
+						if (mTextLength > data.length())
+							continue;
+						// first results that should appear start with the filter
+						if (data.toLowerCase().startsWith(constraint.toString())){
+							mainList.add(data);
+							continue;
+						}
+						// next add results that have multiple words with non-first
+						// starting letter
+						if (data.contains(" ")){
+							String dataArray[] = data.toLowerCase().split(" ");
+							for (String part : dataArray) {
+								if (part.replaceAll("\\(|\\)", "")
+										  .startsWith(constraint.toString())){
+									secondaryList.add(data);
+									continue mainLoop;
+								}
+							}
+						}
+						// finally find matches within words
+						if (data.toLowerCase().contains(constraint)){
+							tertiaryList.add(data);
+						}
+					}
 
-//               // slow down search for testing
-//               for (int j = 0; j < 1000000000; j++) {
-//                  results.count = j;
-//               }
+					// join lists preserving hierarchy and guaranteeing uniqueness
+					mainList.addAll(secondaryList);
+					mainList.addAll(tertiaryList);
 
-               // set the Filtered result to return
-               results.count = FilteredArrList.size();
-               results.values = FilteredArrList;
-            }
-            return results;
-         }
-      };
-   }
+					results.count = mainList.size();
+					results.values = new ArrayList<>(mainList);
+				}
+				return results;
+			}
+		};
+	}
+
+	private class ViewHolder {
+		TextView textView;
+	}
 }
--- app/src/main/java/com/llamacorp/filterdialogexample/MainActivity.java
+++ app/src/main/java/com/llamacorp/filterdialogexample/MainActivity.java
@@ -1,12 +1,9 @@
 package com.llamacorp.filterdialogexample;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-
-import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.Gravity;
@@ -20,7 +17,10 @@ import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 
-public class MainActivity extends Activity {
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
    private final String COUNTRY_LIST[] = {"Afghanistan", "Albania", "Algeria", "Andorra", "Angola", "Anguilla", "Antigua & Barbuda", "Argentina", "Armenia", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia", "Bosnia & Herzegovina", "Botswana", "Brazil", "Brunei Darussalam", "Bulgaria", "Burkina Faso", "Myanmar/Burma", "Burundi", "Cambodia", "Cameroon", "Canada", "Cape Verde", "Cayman Islands", "Central African Republic", "Chad", "Chile", "China", "Colombia", "Comoros", "Congo", "Costa Rica", "Croatia", "Cuba", "Cyprus", "Czech Republic", "Democratic Republic of the Congo", "Denmark", "Djibouti", "Dominican Republic", "Dominica", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia", "Ethiopia", "Fiji", "Finland", "France", "French Guiana", "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Great Britain", "Greece", "Grenada", "Guadeloupe", "Guatemala", "Guinea", "Guinea-Bissau", "Guyana", "Haiti", "Honduras", "Hungary", "Iceland", "India", "Indonesia", "Iran", "Iraq", "Israel and the Occupied Territories", "Italy", "Ivory Coast (Cote d'Ivoire)", "Jamaica", "Japan", "Jordan", "Kazakhstan", "Kenya", "Kosovo", "Kuwait", "Kyrgyz Republic (Kyrgyzstan)", "Laos", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libya", "Liechtenstein", "Lithuania", "Luxembourg", "Republic of Macedonia", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Martinique", "Mauritania", "Mauritius", "Mayotte", "Mexico", "Moldova, Republic of", "Monaco", "Mongolia", "Montenegro", "Montserrat", "Morocco", "Mozambique", "Namibia", "Nepal", "Netherlands", "New Zealand", "Nicaragua", "Niger", "Nigeria", "Korea, Democratic Republic of (North Korea)", "Norway", "Oman", "Pacific Islands", "Pakistan", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Poland", "Portugal", "Puerto Rico", "Qatar", "Reunion", "Romania", "Russian Federation", "Rwanda", "Saint Kitts and Nevis", "Saint Lucia", "Saint Vincent's & Grenadines", "Samoa", "Sao Tome and Principe", "Saudi Arabia", "Senegal", "Serbia", "Seychelles", "Sierra Leone", "Singapore", "Slovak Republic (Slovakia)", "Slovenia", "Solomon Islands", "Somalia", "South Africa", "Korea, Republic of (South Korea)", "South Sudan", "Spain", "Sri Lanka", "Sudan", "Suriname", "Swaziland", "Sweden", "Switzerland", "Syria", "Tajikistan", "Tanzania", "Thailand", "Timor Leste", "Togo", "Trinidad & Tobago", "Tunisia", "Turkey", "Turkmenistan", "Turks & Caicos Islands", "Uganda", "Ukraine", "United Arab Emirates", "United States of America (USA)", "Uruguay", "Uzbekistan", "Venezuela", "Vietnam", "Virgin Islands (UK)", "Virgin Islands (US)", "Yemen", "Zambia", "Zimbabwe"};
    private EditText mItemEditText = null;
    private ArrayList<String> mArraySort;
@@ -30,6 +30,8 @@ public class MainActivity extends Activity {
    protected void onCreate(Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
       setContentView(R.layout.activity_main);
+
+
 
       mItemEditText = (EditText) findViewById(R.id.edit_text_view);
       Button listViewDialogButton = (Button) findViewById(R.id.list_view_dialog_button);
